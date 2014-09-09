ecd377d5f4e69f9b61196d826e04c65d4b380663
Gk <gk@gkparishphilp.com>
2014-09-09 13:35:38 -0700

    - questions with multi-text responses

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -5,13 +5,19 @@ class ResponsesController < ApplicationController
 		# need a surveying
 		# a screen
 		# a prompt
+		df
 		@question = Question.find_by( id: params[:question_id] )
 		
-		@response = @question.responses.where( user_id: current_user.try( :id ) ).first_or_initialize
+		if params[:content].present?
+			params[:content].each do |content|
+				@question.responses.where( user_id: current_user.try( :id ) ).create( content: content )
+			end
+		elsif content = Prompt.find_by( id: params[:prompt_id] ).try( :content )
+			@response = @question.responses.where( user_id: current_user.try( :id ) ).first_or_initialize
+			@response.content = content
+			@response.save
+		end
 		
-		content = params[:content] || Prompt.find_by( id: params[:prompt_id] ).try( :content )
-		@response.content = content
-		@response.save
 
 		if @question.screen.next_screen.present?
 			redirect_to @question.screen.next_screen 
