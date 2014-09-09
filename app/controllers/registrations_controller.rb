c9b07840163ff10bfe7a386948c1c0904d3b4788
Gk <gk@gkparishphilp.com>
2014-09-09 16:04:57 -0700

    - registrations

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,35 @@
+class RegistrationsController < Devise::RegistrationsController
+
+
+	def create
+		email = params[:user][:email]
+		# todo -- check validity of email param?
+
+		user = User.where( email: email ).first || 
+				User.new( email: email, full_name: params[:user][:name], name: params[:user][:name], ip: request.ip )
+
+		if user.encrypted_password.present?
+			# this email is already registered for this site
+			set_flash "#{email} is already registered.", :error
+			redirect_to :back
+			return false
+
+		end
+
+		user.password = params[:user][:password]
+		user.password_confirmation = params[:user][:password_confirmation]
+
+		if user.save
+			#record_app_event( 'registration', on: @current_site, user: user, content: 'registered.' )
+			set_flash "Thanks for signing up!"
+        	sign_up( :user, user )
+        	respond_with user, location: after_sign_up_path_for( user )
+		else
+			set_flash "Could not register user.", :error, user
+			render :new
+			return false
+		end
+
+	end
+
+end
