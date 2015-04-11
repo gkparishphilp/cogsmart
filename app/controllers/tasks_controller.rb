e7a6ef4bab128140a3e779c86a53c5905bc8f180
Daniel Bogart <dbogart@adverseevents.com>
2015-04-11 10:56:47 -0700

    added create and index actions for to do

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,33 @@
 class TasksController < ApplicationController
 
+  before_filter :authenticate_user!
+
+
+  def edit
+
+  end
+
+  def create
+    @task = current_user.tasks.new task_params
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def index
+    @tasks = current_user.tasks
+  end
+
+  def show
+    @tasks = current_user.tasks
+  end
+
+  def new
+    @task = current_user.tasks.new
+  end
+
+  private
+    def task_params
+      params.require( :task ).permit( :name, :description, :due_at )
+    end
+
 end
