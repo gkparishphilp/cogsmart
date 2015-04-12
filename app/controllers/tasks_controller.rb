d6da81650e2dd01b4bbbc17c17715e5403e6dd1e
Daniel Bogart <dbogart@adverseevents.com>
2015-04-12 13:43:57 -0700

    added edit/update/delete functions for todo list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,14 +3,22 @@ class TasksController < ApplicationController
   before_filter :authenticate_user!
 
 
-  def edit
+  def update
+    task = current_user.tasks.find_by_id(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_path
+    flash[:notice] = "Task updated"
+  end
 
+  def edit
+    @task = current_user.tasks.find_by_id(params[:id])
   end
 
   def create
     @task = current_user.tasks.new task_params
     @task.save
     redirect_to tasks_path
+    flash[:notice] = "Task created"
   end
 
   def index
@@ -25,9 +33,16 @@ class TasksController < ApplicationController
     @task = current_user.tasks.new
   end
 
+  def destroy
+    task = current_user.tasks.find_by_id(params[:id])
+    task.destroy!
+    redirect_to tasks_path
+    flash[:notice] = "Task deleted"
+  end
+
   private
     def task_params
-      params.require( :task ).permit( :name, :description, :due_at )
+      params.require( :task ).permit( :name, :description, :due_at, :completed )
     end
 
 end
