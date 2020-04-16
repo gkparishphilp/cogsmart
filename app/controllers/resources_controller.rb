417ed7604f6acaef176da1354b4aaf1b05bfb500
Gk <gk@gkparishphilp.com>
2020-04-16 15:01:48 -0700

    - add mild manual

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,6 +4,7 @@ class ResourcesController < ApplicationController
 	def index
 		@cct_manuals = Resource.by_category( 'cct' )
 		@tbi_manuals = Resource.by_category( 'tbi' )
+		@mild_manuals = Resource.by_category( 'mild' )
 		@addtnl_manuals = Resource.by_category( 'additional' )
 	end
 
